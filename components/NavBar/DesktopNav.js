d2f934417be581cc2387105f6c76c68a66d1ee81
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-01-21 18:22:53 +0200

    created NavBar component

---
+++ components/NavBar/DesktopNav.js
@@ -0,0 +1,12 @@
+const DesktopNav = () => {
+  return (
+    <nav className="flex flex-row justify-around items-end">
+      <div className="flex flex-row justify-self-auto">
+        <ul>
+          <li>about</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default DesktopNav
---
+++ components/NavBar/Index.js
@@ -0,0 +1,15 @@
+import DesktopNav from "./DesktopNav"
+import Image from "next/image"
+import Bunny from "../../public/darkbunny.png"
+const NavBar = () => {
+  return (
+    <nav className="flex flex-row justify-between items-center pt-3">
+      <h1 className=" mx-7 font-Quick font-bold text-jazzberry-jam text-xl hidden">
+        <Image src={Bunny} ariaHidden="true" /> Milican portfolio
+      </h1>
+      <DesktopNav />
+    </nav>
+  )
+}
+
+export default NavBar
