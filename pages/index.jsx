d4392aa410716e78eab790f09704b4ce8c50d0e4
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-01-25 17:57:06 +0200

    import new component

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero"
 import About from "../components/About"
 import RecentWork from "../components/RecentWork"
 import More from "../components/More"
+import Testimonials from "../components/Testimonials"
 import NavBar from "../components/NavBar/Index"
 
 export default function Home() {
@@ -28,6 +29,7 @@ export default function Home() {
         <About />
         <RecentWork />
         <More />
+        <Testimonials />
       </main>
 
       <footer className=""></footer>
