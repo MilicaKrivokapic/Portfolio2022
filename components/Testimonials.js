adc9da00822ede3dbe86fde946fc1dc4b650cff8
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-01-25 17:59:15 +0200

    created Testimonials section

---
+++ components/Testimonials.js
@@ -0,0 +1,10 @@
+export default function Testimonials() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2 className="font-bold text-2xl font-Quick mb-4"> Suosituksia </h2>
+      <p className="font-Work">
+        Tyypit joiden kanssa olen työskennelleet haluavat sanoa sanasen
+      </p>
+    </div>
+  )
+}
