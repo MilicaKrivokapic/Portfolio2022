cc18ab8e19975b20b3a7974e3f441818a8cd1533
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-02-02 13:46:59 +0200

    edited SliderButton

---
+++ components/Buttons/SliderButton.js
@@ -0,0 +1,8 @@
+export default function SliderButton() {
+  return (
+    <button
+      type="button"
+      className="absolute z-40 bg-purple-900/50 h-10 w.40 rounded-full flex items-center justify-center"
+    ></button>
+  )
+}
