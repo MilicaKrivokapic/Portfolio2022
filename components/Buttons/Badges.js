edaeaeb5ff01f6a2b4ee646f2bd45398e1b2c432
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-02-05 20:17:40 +0200

    changed "class" to "className" :D this keeps happening to me....

--- components/Buttons/Badges.js
+++ components/Buttons/Badges.js
@@ -1,6 +1,6 @@
 export const ReactBadge = () => {
   return (
-    <span class="bg-indigo-100 font-Work text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900">
+    <span className="bg-indigo-100 font-Work text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900">
       React
     </span>
   )
@@ -8,7 +8,7 @@ export const ReactBadge = () => {
 
 export const WordpressBadge = () => {
   return (
-    <span class=" bg-green-100 text-emerald-600  text-sm font-Work font-semibold mr-2 px-2.5 py-0.5 rounded">
+    <span className=" bg-green-100 text-emerald-600  text-sm font-Work font-semibold mr-2 px-2.5 py-0.5 rounded">
       Wordpress
     </span>
   )
@@ -16,7 +16,7 @@ export const WordpressBadge = () => {
 
 export const TailwindBadge = () => {
   return (
-    <span class="bg-cyan-100 text-cyan-500 font-Work text-sm font-semibold mr-2 px-2.5 py-0.5 rounded ">
+    <span className="bg-cyan-100 text-cyan-500 font-Work text-sm font-semibold mr-2 px-2.5 py-0.5 rounded ">
       Tailwind
     </span>
   )
@@ -24,21 +24,21 @@ export const TailwindBadge = () => {
 
 export const NextBadge = () => {
   return (
-    <span class="bg-pink-100 text-pink-500 font-Work text-sm font-semibold mr-2 px-2.5 py-0.5 rounded ">
+    <span className="bg-pink-100 text-pink-500 font-Work text-sm font-semibold mr-2 px-2.5 py-0.5 rounded ">
       Next.js
     </span>
   )
 }
 export const CSharpBadge = () => {
   return (
-    <span class="bg-violet-100 text-violet-500 font-Work text-sm font-semibold mr-2 px-2.5 py-0.5 rounded ">
+    <span className="bg-violet-100 text-violet-500 font-Work text-sm font-semibold mr-2 px-2.5 py-0.5 rounded ">
       C#
     </span>
   )
 }
 export const UIBadge = () => {
   return (
-    <span class="bg-amber-100 text-amber-600 font-Work text-sm font-semibold mr-2 px-2.5 py-0.5 rounded ">
+    <span className="bg-amber-100 text-amber-600 font-Work text-sm font-semibold mr-2 px-2.5 py-0.5 rounded ">
       Design
     </span>
   )
