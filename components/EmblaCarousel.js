230213279d3b0a4ea231d68ee0d26cf0cb9f0f3a
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-02-07 14:09:10 +0200

    fixed some odd css on carousel

--- components/EmblaCarousel.js
+++ components/EmblaCarousel.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from "react"
-import { DotButton, PrevButton, NextButton } from "./EmblaCarouselButtons"
 import useEmblaCarousel from "embla-carousel-react"
 import { Thumb } from "./EmblaCarouselThumb"
 
