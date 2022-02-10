e331978d8850f5921cdbd95bf5164cdb2c99ad8d
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-02-10 18:39:30 +0200

    Added pink shadow to the huge M-letter

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,24 +1,19 @@
-import Image from "next/image"
-import Milica from "../components/img/circle_portfolio.png"
-import RabbitHole from "../components/img/rabbithole.gif"
 const Hero = () => {
   return (
     <div className="grid grid-cols-1 grid-rows-1 items-center justify-items-center overflow-hidden h-screen xl:h-screen">
-      <div className=" col-start-1 col-span-1 row-span-1 row-start-1 xl:-mt-[27rem] xl:-mb-[17rem] font-Work font-semibold text-[120vw] lg:text-[70vw] xl:text-[60vw] z-20 text-jazzberry-jam-200/20 mix-blend-color max-w-screen translate-x-2">
+      <div className=" col-start-1 col-span-1 row-span-1 row-start-1 xl:-mt-[27rem] xl:-mb-[17rem] font-Work font-semibold text-[120vw] lg:text-[70vw] xl:text-[60vw]  mix-blend-overlay  text-fuchsia-blue-600/20 z-30">
         M
       </div>
-      <div className=" col-start-1 col-span-1 row-span-1 row-start-1 xl:-mt-[27rem] xl:-mb-[17rem] font-Work font-semibold text-[120vw] lg:text-[70vw] xl:text-[60vw] z-10 text-jazzberry-jam-700/50 mix-blend-overlay translate-x-4">
-        M
-      </div>
-      <div className=" col-start-1 col-span-1 row-span-1 row-start-1 xl:-mt-[27rem] xl:-mb-[17rem] font-Work font-semibold text-[120vw] lg:text-[70vw] xl:text-[60vw] translate-x-1 mix-blend-hue  text-fuchsia-blue-600/20">
+      <div className=" col-start-1 col-span-1 row-span-1 row-start-1 xl:-mt-[27rem] xl:-mb-[17rem] font-Work font-semibold text-[120vw] lg:text-[70vw] xl:text-[60vw] z-10 text-jazzberry-jam-100/30 mix-blend-darken bg-blend-overlay max-w-screen translate-x-2">
         M
       </div>
       <div
         className="col-start-1 col-span-1 row-span-1 row-start-1"
         style={{
           backgroundImage: "url(astro.gif)",
-          backgroundSize: "cover",
+          backgroundSize: "contain",
           backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
         }}
       >
         <div className=" col-start-1 col-span-1 row-span-1 row-start-1 xl:-mt-[27rem] xl:-mb-[17rem] font-Work font-semibold text-black mix-blend-screen bg-white text-[120vw] lg:text-[70vw] xl:text-[60vw] ">
