1fbf6018c1fa1632a313b112625818536dee2139
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-02-11 13:41:06 +0200

    added txt and deleted some useless code

--- components/More.jsx
+++ components/More.jsx
@@ -4,11 +4,8 @@ import Image from "next/image"
 export default function More() {
   return (
     <section>
-      {/* <div className="flex items-center justify-center mt-12">
-        <div className="flex items-center justify-center w-5/6 h-24 border-t"></div>
-      </div> */}
       <div className="w-full flex justify-center">
-        <div className="flex lg:flex-row-reverse flex-col justify-around mb-32 bg-gradient-to-b md:bg-gradient-to-l from-[#010307] to-[#00003A] text-white py-20 w-full px-7 xl:px-20 lg:w-5/6 lg:rounded-2xl mt-40">
+        <div className="flex lg:flex-row-reverse flex-col justify-around mb-32 bg-gradient-to-b lg:bg-gradient-to-l from-[#010307] to-[#00003A] text-white py-20 w-full px-7 xl:px-20 lg:w-5/6 lg:rounded-2xl mt-40">
           <div className="grid grid-cols-1 grid-rows-1 h-full items-center justify-items-center xl:justify-items-start xl:items-center ml-10 -mt-36 sm:-mt-44 lg:-mt-0">
             <div
               className="col-start-1 col-span-1 row-span-1 row-start-1 -translate-x-12 w-[150px] h-[150px] md:w-[200px] md:h-[200px]"
@@ -38,57 +35,65 @@ export default function More() {
             <h2 className="text-center text-3xl font-Quick font-semibold pb-7 py-14 ">
               Lisää minusta
             </h2>
-            <div
-              className="lg:px-10 lg:pr-24 "
-              // className="[clip-path:circle(50%_at_20%_30%)]
-              //   [shape-outside:circle(50%_at_20%_30%)]"
-            >
-              <p>
-                A wonderful serenity has taken possession of my entire soul,
-                like these sweet mornings of spring which I enjoy with my whole
-                heart. I am alone, and feel the charm of existence in this spot,
-                which was created for the bliss of souls like mine. I am so
-                happy, my dear friend, so absorbed in the exquisite sense of
-                mere tranquil existence, that I neglect my talents.
+            <div className="lg:px-10 lg:pr-24 md:text-lg lg:text-xl lg:leading-8">
+              <p className="mb-2">Webdevaaminen on kivaa!</p>
+              <p className="mb-4">
+                Jos saisin valita yhden asian, jonka pelastaisin (puolisoni ja
+                koiranpennun lisäksi, tietenkin) palavasta talosta, valitsisin
+                tietokoneen. Pelaaminen, kirjoittaminen ja tiedostojen ulkonäön
+                muokkaaminen ovat olleet minulle lapsesta saakka tärkeitä
+                asioita.
+              </p>
+              <p className="mb-4">
+                Työhistoriani on ollut yksi haarautuvien polkujen puutarha -
+                olen kokeillut nuoruudessani harjoittelujaksojen avulla mm.
+                vanhustyötä, aakkostanut opuksia kirjastotätinä, huhkinut
+                teollisuustiskikoneen ääressä suurkeittiössä, työskennellyt vain
+                valkoisia esineitä myyvässä sisustuskaupassa... You name it.
+                Minusta tuntui, etten koskaan oikein sopinut joukkoon, koska
+                juutuin aina tietokoneen äärelle - myyntityössä minua kiinnosti
+                enemmän tavaroiden järjestely ja arviointi, kuin varsinainen
+                markkinoiva myyntityö.
+              </p>
+              <p className="mb-4">
+                Ystävieni tuella ja kannustuksella ja uskalsin lopulta hakea
+                media-alan peruskoulutukseen. Tämä muutti elämäni, sillä
+                koulussa löysin ensimmäisen oikean intohimoni: valokuvauksen.
+                Rakastin aivan erityisesti valokuvien muokkaamista Photoshopilla
+                sekä videoiden jälkikäsittelyä After Effectsillä. Koulussa opin
+                myös webdevaamisen varhaiset alkeet.
+              </p>
+              <p className="mb-4">
+                Liityin media-alan osuuskuntaan, ja aloin tehdä erinäisiä
+                keikkatöitä; olen mm. ollut kuvaamassa vanhoille
+                punk-legendoille musiikkivideoita, kuvannut tapahtumia (kuten
+                häitä ja messuja) sekä ääninäytellyt peleissä. Innostuin kovasti
+                myös 3d-mallintamisesta, josta olisin halunnut saada itselleni
+                joskus työpaikan, mutta alan realititeetit ensimmäisen pidemmän
+                työharjoittelurupeaman ja verkostointitapahtumissa tapaamieni
+                ihmisten kertomusten jälkeen intoni pelia-alalla työskentelystä
+                alkoi laimentua. Media-alan osuuskuntamme ajautui konkurssiin
+                ensimmäisenä koronavuonna, ja päätin kokeilla jotain uutta ja
+                aivonystyröitä haastavaa: webdevaamista. Devaamisen kanssa kävin
+                samoin kuin valokuvauksen, jäin heti ensimmäisestä keikasta
+                koukkuun. Pidän silti yhä 3D-maailmasta, ja toivon voivani vielä
+                yhdistää sen ja webdevaamisen - Three.js-kirjasto kiinnostaa
+                erityisesti.
+              </p>
+              <p className="mb-4">
+                Tavoitteenani on oppia ihan joka päivä jotain uutta, ja ymmärtää
+                holistisesti verkkosivujen toimintaa ja rakentamista. Front-end
+                on minun heiniäni, mutta haaveilen ajan kanssa tulevani
+                kiitettäväksi Fullstack-osaajaksi. Olen valmiina kaikenlaisiin
+                uusiin haasteisiin!
               </p>
 
-              <p>
-                I should be incapable of drawing a single stroke at the present
-                moment; and yet I feel that I never was a greater artist than
-                now.
-              </p>
-
-              <p>When, while the lovely valley teems with</p>
-
-              <div className="">
-                <p>
-                  A wonderful serenity has taken possession of my entire soul,
-                  like these sweet mornings of spring which I enjoy with my
-                  whole heart.
+              <div className=" font-Quick font-semibold text-center mt-10">
+                <p className="font-Parisienne font-light text-4xl">
+                  Milica Krivokapic
                 </p>
-
-                <p>
-                  I am alone, and feel the charm of existence in this spot,
-                  which was created for the bliss of souls like mine.
-                </p>
-
-                <p>
-                  I am so happy, my dear friend, so absorbed in the exquisite
-                  sense of mere tranquil existence, that I neglect my talents.
-                </p>
-
-                <p>
-                  I should be incapable of drawing a single stroke at the
-                  present moment; and yet I feel that I never was a greater
-                  artist than now.
-                </p>
-
-                <p>When, while the lovely valley teems with</p>
-
-                <div className=" font-Quick font-semibold text-center mt-10">
-                  Haluatko jatkaa tutustumista ja jutella lisää? Ota yhteyttä
-                  <p>yhteyslinkki</p>
-                </div>
+                <p>Haluatko jatkaa tutustumista ja jutella lisää? </p>Ota
+                yhteyttä: yhteyslinkki
               </div>
             </div>
           </div>
