bc4cea1d04cf89f99e29a3919c8c27a8a1b08da5
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-02-11 13:41:31 +0200

    added footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Head from "next/head"
+import Footer from "../components/Footer"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,11 +15,12 @@ function MyApp({ Component, pageProps }: AppProps) {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Parisienne&family=Quicksand:wght@300;400;500;600;700&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         ></link>
       </Head>
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
