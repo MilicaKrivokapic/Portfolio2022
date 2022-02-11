c8079d454a9213d1354218fc0f434d6e03863654
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-02-11 13:16:38 +0200

    carousel no longer causes overflow

--- components/Carousel.js
+++ components/Carousel.js
@@ -48,8 +48,8 @@ export default function Carousel() {
     <button
       type="button"
       onClick={isLeft ? previousImage : nextImage}
-      className={`absolute text-white text-2xl z-10  h-10 w-10 flex items-center justify-center top-[40%] ${
-        isLeft ? "left-2" : "right-2"
+      className={`absolute text-white text-2xl z-10  h-10 w-10 flex items-center justify-center top-[40%]  ${
+        isLeft ? "xl:left-80 left-2 " : "xl:right-80 right-2"
       }`}
     >
       <span role="img" aria-label={`Arrow ${isLeft ? "left" : "right"}`}>
@@ -59,7 +59,7 @@ export default function Carousel() {
   )
 
   return (
-    <div className="p-12 flex justify-center w-screen items-center">
+    <div className="p-12 flex justify-center w-full items-center">
       <div className="relative w-full">
         <div
           className="w-full"
@@ -80,8 +80,8 @@ export default function Carousel() {
                 ref={refs[i]}
                 className="w-full flex-shrink-0 rounded-full flex flex-col items-center justify-center"
               >
-                <div className="lg:w-[18%] w-[50%] sm:w-[38%] p-3 bg-gradient-to-tl from-indigo-900 to-sky-600 rounded-full">
-                  <img className="rounded-full " src={comment.img.src} />
+                <div className="lg:w-[18%] w-[50%] sm:w-[38%] p-3 rounded-full bg-transparent shadow-indigo-300/80 shadow-md ">
+                  <img className="rounded-full" src={comment.img.src} />
                 </div>
                 <span className="font-Work text-lg mt-4">{comment.txt}</span>
               </div>
