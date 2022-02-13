c470a59721c1ea578429e30c6d2b397ac9144489
Milica Krivokapic <milica1krivokapic@gmail.com>
2022-02-13 13:06:05 +0200

    new comments

--- components/WorkCard.jsx
+++ components/WorkCard.jsx
@@ -10,6 +10,8 @@ export default function WorkCard({
 }) {
   return (
     <>
+      {/* I used my classical tactic here, which kinda is the founding theme for this portfolio: one column grid with all the things in the same column. In that way everything is neatly stacked, no need for messy chaos called position:absolute */}
+
       <div
         className={`grid grid-cols-1 grid-rows-1 aspect-[3/2] rounded-2xl overflow-hidden`}
       >
@@ -24,7 +26,9 @@ export default function WorkCard({
         >
           <div className={` ${cardColor} w-full h-full  `} />
         </div>
+
         {/* the class "blurred" is defined in globals.css, therefore it is not a Tailwind class, but my own fallback for Firefox. Surprisingly Firefox doesn't support background filter rule, as the other browsers do" */}
+
         <div className="opacity-0 hover:opacity-100 bg-transparent h-full w-full flex flex-col items-center justify-center row-start-1 row-span-1 col-start-1 col-span-1 p-6 blurred transition duration-500 ease-out">
           {children}
           <h3 className="text-white text-center font-Work font-bold mb-6 text-2xl drop-shadow-2xl xl:mb-13 ">
